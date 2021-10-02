2fce7c9fa7146af1de0e61bbe12ac7ea2939b156
DeValmeida <79146033+valber4513@users.noreply.github.com>
2021-10-02 18:01:46 -0300

    IMPORTANCIA DO Código TRY, Serve para Encontrar qualquer erro no sistema imprimindo ele no final.

--- projetojava/src/projetojavapacote/moduloObjeto1.java
+++ projetojava/src/projetojavapacote/moduloObjeto1.java
@@ -7,25 +7,31 @@ import projetojavapacote.classesauxiliares.FuncaoAutenticacao;
 public class moduloObjeto1 {
 
 	public static void main(String[] args) {
-
-		/*  INTERFACES E CONSTRUTORES
-		 *
-		 */
+		
+		try {/* IMPORTANCIA DO TRY, dentro dele  fica toda regra de negocio, com esse código você poderá
+		capturar qualqur erro e corrigir de forma mais rapida */
+		
+		
 		String login = JOptionPane.showInputDialog(" Informe o Login ");
 		String senha = JOptionPane.showInputDialog("Informe a Senha ");
+
 		
-	
-		
-		
-	/* Será Travado o contrato para autorizar somente quem realmente tem 
-	 * o contrato 100% Regitimo
-	 * será feito uma classe a mais que vai receber qualquer classe que implemente essa interface */
-		if (new FuncaoAutenticacao( new Diretor(senha, senha)).autenticar()) { /* vamos travar o contrato para autorizar somente quem tem o contrato 100% legitimom*/
+		if (new FuncaoAutenticacao(new Diretor(senha, senha))
+				.autenticar()) { 
+									 
 			JOptionPane.showMessageDialog(null, "Carregando Seu Sistema");
 			System.out.println("Acesso Concluído !! Bem vindo ao Inicio de Tudo Programador");
 
-		}else {
-			JOptionPane.showMessageDialog(null , "Login ou Senha Incorreto");
+		} else {
+			JOptionPane.showMessageDialog(null, "Login ou Senha Incorreto");
+		}
 		
+		
+		}catch (Exception e) {/*fechamos o Código com (Exception e, e.printStackTrace), aqui o Sistema ira o local do erro, isso é fundamental
+		para o programador encontrar o erro e corrigir, caso não seja feito será necessário olhar linha por linha*/
+		e.printStackTrace();
+		
+		}
+
 	}
-}}
+}
