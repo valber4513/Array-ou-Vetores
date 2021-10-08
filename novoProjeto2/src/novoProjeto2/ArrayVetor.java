5fb9826f6db209e9def44fe370016acad7b2ffec
DeValmeida <79146033+valber4513@users.noreply.github.com>
2021-10-07 23:00:52 -0300

    atribuindo valores a Array

---
+++ novoProjeto2/src/novoProjeto2/ArrayVetor.java
@@ -0,0 +1,30 @@
+package novoProjeto2;
+
+public class ArrayVetor {
+
+	public static void main(String[] args) {
+		
+/*Primeira Aula de Array */
+		
+		/*Array pode ser de todos os tipos de dados e Objetos */
+		
+		
+		/* Criando uma array, elas são reconhecidas por terem [ no inicio e , ] no fim. No exemplo abaixo foram criadas
+		 * 4 posições contando com o  (0 )*/ 
+		double [] notas = new double [4];
+		
+	
+		notas [ 0] = 9.8; /*Atribuindo um valor a uma array */
+		notas [1] = 8.7;
+		notas [2] = 9.7;
+		notas [3]= 9.9;
+		
+		
+		
+		System.out.println(notas);
+	}
+	
+		
+	}
+
+
