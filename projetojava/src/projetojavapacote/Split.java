ad742cb6c9203a13f6a7b38d5598edc0f4c9ec39
DeValmeida <79146033+valber4513@users.noreply.github.com>
2021-10-24 14:27:15 -0300

    Convertendo um Array em uma Lista e Convertendo uma lista Para Array

--- projetojava/src/projetojavapacote/Split.java
+++ projetojava/src/projetojavapacote/Split.java
@@ -1,5 +1,8 @@
 package projetojavapacote;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Split {
 
 public static void main(String[] args) {
@@ -11,10 +14,25 @@ public static void main(String[] args) {
 	e utilizamos   .split para fazer a leitura separado pelos sinais  ","  */
 	
 	System.out.println("Nome: "+ valoresArray [0]);
-	System.out.println("Treinamento: "+ valoresArray [1]);
+	System.out.println("Treinamento: "+valoresArray [1]);
 	System.out.println("Nota 1: "+ valoresArray [2]);
 	System.out.println("Nota 2: "+ valoresArray [3]);
 	System.out.println("Nota 3: "+ valoresArray [4]);
+	
+	/*Convertendo um Array em uma Lista */
+	
+	List<String> list = Arrays.asList(valoresArray);
+	
+	for(String valorString : list) {
+		System.out.println(valorString);
+	}
+	
+	/* Convertendo uma lista Para Array */
+	
+	String [] conversaoArray = list.toArray(new String [5]);
+	for(int pos = 0; pos < conversaoArray.length; pos++) {
+		System.out.println(conversaoArray[pos]);
+	}
 	
 	
 	}
